028b49397b6a0645bddf0c00f184e0635128922c
anthonyEShay <35188695+anthonyEShay@users.noreply.github.com>
2018-03-07 12:21:52 -0800

    Add files via upload

---
+++ buffer.h
@@ -0,0 +1,15 @@
+/*
+ * buffer.h
+ *
+ * Author: Anthony Shay
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+typedef int buffer_item;
+#define BUFFER_SIZE 5
+
+
+
+#endif /* BUFFER_H_ */
---
+++ makefile
@@ -0,0 +1,3 @@
+#OS Program 3
+Pro3: Pro3.cpp
+	g++ -pthread -o Pro3 -std=c++11 Pro3.cpp -I.
